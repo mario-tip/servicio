462af4a8e73e11fcb9ecfb8f765054d8143b7feb
marfelix <felix.mar@hotmail.com>
2018-11-06 19:06:03 -0600

    Fin del dia

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -9,6 +9,367 @@
 
   <div id="dashboard" class="row">
 
+    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 ">
+			<!-- BEGIN Portlet PORTLET-->
+			<div class="portlet gren">
+				<div id="flex_icon" class="portlet-title">
+					<div class="caption tools">
+            <div class="tiles collapse" >
+              <a href="" >
+                <div class="tile bg-green minizoom">
+                  <div class="tile-body">
+                    <i class="icon-earphones-alt"></i>
+                  </div>
+                  <div class="tile-object">
+                    <div class="text-center">
+                      <h4> <strong> Help Desk </strong> </h4>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+					</div>
+				</div>
+				<div class="portlet-body" >
+          <div class="tiles">
+            <a href="{!!URL::to('/incidents')!!}">
+              <div class="tile bg-green-turquoise minizoom">
+                <div class="tile-body">
+                  <i class="fa fa-exclamation-triangle"></i>
+                </div>
+                <div class="tile-object">
+                  <div class="text-center">
+                    <h4> <strong> Incidents </strong> </h4>
+                  </div>
+                </div>
+              </div>
+            </a>
+
+            <a href="{!!URL::to('/help_service')!!}">
+              <div class="tile bg-blue-madison minizoom">
+                <div class="tile-body">
+                  <i class="fa fa-wrench"></i>
+                </div>
+                <div class="tile-object">
+                  <div class="text-center">
+                    <h4><strong>Service</strong></h4>
+                  </div>
+                </div>
+              </div>
+            </a>
+
+            <a href="{!!URL::to('maintenances')!!}">
+              <div class="tile bg-grey-cascade minizoom">
+                <div class="tile-body">
+                  <i class="fa fa-calendar"></i>
+                </div>
+                <div class="tile-object">
+                  <div class="text-center">
+                    <h4> <strong>Maintenance </strong> </h4>
+                  </div>
+                </div>
+              </div>
+            </a>
+
+            <a href="{!!URL::to('/')!!}">
+              <div class="tile bg-grey-cascade minizoom">
+                <div class="tile-body">
+                  <i class="icon-bell"></i>
+                </div>
+                <div class="tile-object">
+                  <div class="text-center">
+                    <h4> <strong>Problems </strong> </h4>
+                  </div>
+                </div>
+              </div>
+            </a>
+
+          </div>
+				</div>
+			</div>
+			<!-- END Portlet PORTLET-->
+		</div>
+
+    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 ">
+			<!-- BEGIN Portlet PORTLET-->
+			<div class="portlet gren">
+				<div id="flex_icon" class="portlet-title">
+					<div class="caption tools">
+            <div class="tiles collapse" >
+              <a href="" >
+                <div class="tile bg-green minizoom">
+                  <div class="tile-body">
+                    <i class="icon-screen-desktop"></i>
+                  </div>
+                  <div class="tile-object">
+                    <div class="text-center">
+                      <h4> <strong>Assets</strong> </h4>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+					</div>
+				</div>
+				<div class="portlet-body" >
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
+            <div class="portlet-body">
+              <div class="tiles">
+                  <a href="{!!URL::to('/equipments')!!}">
+                      <div class="tile bg-blue minizoom">
+
+                          <div class="tile-body">
+                              <i class="icon-notebook" ></i>
+                          </div>
+                          <div class="tile-object">
+                              <div class="text-center">
+                                  <h4> <strong> Equipment  </strong> </h4>
+                              </div>
+                          </div>
+                      </div>
+                  </a>
+
+                  <a href="{!!URL::to('/actives')!!}">
+                      <div class="tile bg-blue-hoki minizoom">
+                          <div class="tile-body">
+                              <i class="icon-login"></i>
+                          </div>
+                          <div class="tile-object">
+                              <div class="text-center">
+                                  <h4> <strong> Asset List </strong> </h4>
+                              </div>
+                          </div>
+                      </div>
+                  </a>
+
+                  <a href="{!!URL::to('/quotations')!!}">
+                      <div class="tile bg-blue-steel minizoom">
+                          <div class="tile-body">
+                            <i class="fa fa-quote-left"></i>
+                          </div>
+                          <div class="tile-object">
+                              <div class="text-center">
+                                  <h4> <strong> Service </strong> </h4>
+                              </div>
+                          </div>
+                      </div>
+                  </a>
+
+                  <a href="{!!URL::to('/service-orders')!!}">
+                    <div class="tile bg-blue-madison minizoom">
+                      <div class="tile-body">
+                        <i class="fa fa-wrench"></i>
+                      </div>
+                      <div class="tile-object">
+                        <div class="text-center">
+                          <h4><strong>Service</strong></h4>
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+              </div>
+            </div>
+          </div>
+				</div>
+			</div>
+			<!-- END Portlet PORTLET-->
+		</div>
+
+    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 ">
+			<!-- BEGIN Portlet PORTLET-->
+			<div class="portlet gren">
+				<div id="flex_icon" class="portlet-title">
+					<div class="caption tools">
+            <div class="tiles collapse" >
+              <a href="" >
+                <div class="tile bg-green minizoom">
+                  <div class="tile-body">
+                    <i class="icon-graph"></i>
+                  </div>
+                  <div class="tile-object">
+                    <div class="text-center">
+                      <h4> <strong>Analytics</strong> </h4>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+					</div>
+				</div>
+				<div class="portlet-body" >
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
+            <div class="portlet-body">
+              <div class="tiles">
+                <a href="{!!URL::to('/equipments')!!}">
+                  <div class="tile bg-blue minizoom">
+                    <div class="tile-body">
+                        <i class="icon-notebook" ></i>
+                    </div>
+                    <div class="tile-object">
+                      <div class="text-center">
+                        <h4> <strong> Equipment  </strong> </h4>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+
+                <a href="{!!URL::to('/actives')!!}">
+                  <div class="tile bg-blue-hoki minizoom">
+                    <div class="tile-body">
+                      <i class="icon-login"></i>
+                    </div>
+                    <div class="tile-object">
+                      <div class="text-center">
+                        <h4> <strong> Asset List </strong> </h4>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+
+                <a href="{!!URL::to('/quotations')!!}">
+                  <div class="tile bg-blue-steel minizoom">
+                    <div class="tile-body">
+                      <i class="fa fa-quote-left"></i>
+                    </div>
+                    <div class="tile-object">
+                      <div class="text-center">
+                        <h4> <strong> Service </strong> </h4>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+
+                <a href="{!!URL::to('/service-orders')!!}">
+                  <div class="tile bg-blue-madison minizoom">
+                    <div class="tile-body">
+                      <i class="fa fa-wrench"></i>
+                    </div>
+                    <div class="tile-object">
+                      <div class="text-center">
+                        <h4><strong>Service</strong></h4>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+				</div>
+			</div>
+			<!-- END Portlet PORTLET-->
+		</div>
+
+    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 ">
+			<!-- BEGIN Portlet PORTLET-->
+			<div class="portlet gren">
+				<div id="flex_icon" class="portlet-title">
+					<div class="caption tools">
+            <div class="tiles collapse" >
+              <a href="" >
+                <div class="tile bg-green minizoom">
+                  <div class="tile-body">
+                    <i class="icon-settings"></i>
+                  </div>
+                  <div class="tile-object">
+                    <div class="text-center">
+                      <h4> <strong>Admin Panel</strong> </h4>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+					</div>
+				</div>
+				<div class="portlet-body" >
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
+            <div class="portlet-body">
+              <div class="tiles">
+                <a href="{!!URL::to('/equipments')!!}">
+                  <div class="tile bg-blue minizoom">
+                    <div class="tile-body">
+                        <i class="icon-notebook" ></i>
+                    </div>
+                    <div class="tile-object">
+                      <div class="text-center">
+                        <h4> <strong> Equipment  </strong> </h4>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+
+                <a href="{!!URL::to('/actives')!!}">
+                  <div class="tile bg-blue-hoki minizoom">
+                    <div class="tile-body">
+                      <i class="icon-login"></i>
+                    </div>
+                    <div class="tile-object">
+                      <div class="text-center">
+                        <h4> <strong> Asset List </strong> </h4>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+
+                <a href="{!!URL::to('/quotations')!!}">
+                  <div class="tile bg-blue-steel minizoom">
+                    <div class="tile-body">
+                      <i class="fa fa-quote-left"></i>
+                    </div>
+                    <div class="tile-object">
+                      <div class="text-center">
+                        <h4> <strong> Service </strong> </h4>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+
+                <a href="{!!URL::to('/service-orders')!!}">
+                  <div class="tile bg-blue-madison minizoom">
+                    <div class="tile-body">
+                      <i class="fa fa-wrench"></i>
+                    </div>
+                    <div class="tile-object">
+                      <div class="text-center">
+                        <h4><strong>Service</strong></h4>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+				</div>
+			</div>
+			<!-- END Portlet PORTLET-->
+		</div>
+      {{-- <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+      <a class="dashboard-stat dashboard-stat-v2 blue" href="#">
+          <div class="visual">
+              <i class="fa fa-users"></i>
+          </div>
+          <div class="details">
+              <div class="number">
+                  <span data-counter="counterup" data-value="0">0</span>
+              </div>
+              <div class="desc">Usuarios</div>
+          </div>
+      </a>
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+      <a class="dashboard-stat dashboard-stat-v2 red" href="#">
+          <div class="visual">
+              <i class="fa fa-list"></i>
+          </div>
+          <div class="details">
+              <div class="number">
+                  <span data-counter="counterup" data-value="0">0</span>
+              </div>
+              <div class="desc">Nombre de catálogo</div>
+          </div>
+      </a>
+    </div> --}}
+
+
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
       <div class="portlet-body">
         <div class="tiles">
@@ -175,32 +536,9 @@
         </div>
       </div>
     </div>
-    {{-- <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-      <a class="dashboard-stat dashboard-stat-v2 blue" href="#">
-          <div class="visual">
-              <i class="fa fa-users"></i>
-          </div>
-          <div class="details">
-              <div class="number">
-                  <span data-counter="counterup" data-value="0">0</span>
-              </div>
-              <div class="desc">Usuarios</div>
-          </div>
-      </a>
-    </div>
-    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-      <a class="dashboard-stat dashboard-stat-v2 red" href="#">
-          <div class="visual">
-              <i class="fa fa-list"></i>
-          </div>
-          <div class="details">
-              <div class="number">
-                  <span data-counter="counterup" data-value="0">0</span>
-              </div>
-              <div class="desc">Nombre de catálogo</div>
-          </div>
-      </a>
-    </div> --}}
+
+
+
   </div>
 
 @endsection
